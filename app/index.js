8882de13534c9e059a6c8c523c7761f2229e03c1
alireza kariminejad <alireza.kariminejad@yahoo.com>
2020-05-21 17:44:26 +0430

    Bootstrap Template Engine

--- app/index.js
+++ app/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+require("./bootstrap")(app);
+
+app.get("/", (req, res) => {
+  res.render("main", { layout: false, userid: 09 });
+});
 
 module.exports = () => {
   const port = process.env.APP_PORT;
-  app.listen(8080, () => {
+  app.listen(5000, () => {
     console.log(`app is listening on port ${port}`);
   });
 };
